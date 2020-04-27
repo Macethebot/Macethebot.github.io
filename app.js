8c3d55a635ad389a1b9513328fcc8f44ee58c9c5
Macethebot <macimorris20@gmail.com>
2020-04-27 14:56:59 -0400

    Updating site

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-﻿
+﻿/*
+   // this is how to comment in JavaScript
+*/
+
+'use strict';
+console.log('Here\'s a hidden message');
+
+let today = new Date();
+let formateDate = today.toDateString();
+let selectElememt = document.getElementById('date');
+selectElememt.innerHTML = formateDate;
+
