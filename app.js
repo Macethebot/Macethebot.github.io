d829f928b9095f25d1a732d9f9e2d330723a9e67
Macethebot <macimorris20@gmail.com>
2020-05-08 13:20:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 'use strict';
 console.log('Here\'s a hidden message');
-
+// addinng comment 
 let today = new Date();
 let formateDate = today.toDateString();
 let selectElememt = document.getElementById('date');
